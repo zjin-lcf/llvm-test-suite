7f0e3f0876f483790f47e29e5441683e56cb5497
Vyacheslav Klochkov <vyacheslav.n.klochkov@intel.com>
2022-10-06 10:23:08 -0500

    Revert "[SYCL][ESIMD]Add tests for support for different types for lsc functions (#1305)"
    This reverts commit eb4ecde3e2077cd064186cfc83deb540aac138cd.

--- SYCL/ESIMD/lsc/Inputs/common.hpp
+++ SYCL/ESIMD/lsc/Inputs/common.hpp
@@ -7,19 +7,12 @@
 //===----------------------------------------------------------------------===//
 
 #include <stdlib.h>
-#include <sycl/bit_cast.hpp>
 
 template <int case_num> class KernelID;
 
 template <typename T> T get_rand() {
-  using Tuint = std::conditional_t<
-      sizeof(T) == 1, uint8_t,
-      std::conditional_t<
-          sizeof(T) == 2, uint16_t,
-          std::conditional_t<sizeof(T) == 4, uint32_t,
-                             std::conditional_t<sizeof(T) == 8, uint64_t, T>>>>;
-  Tuint v = rand();
-  if constexpr (sizeof(Tuint) > 4)
+  T v = rand();
+  if constexpr (sizeof(T) > 4)
     v = (v << 32) | rand();
-  return sycl::bit_cast<T>(v);
+  return v;
 }
